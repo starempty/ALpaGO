b2b904a203e488fa718facc46f996d8bc65980a8
한주연 <taxol1203@naver.com>
2021-08-11 18:12:00 +0900

    123

--- week9-tree+알파/taxol/bj_12851_숨바꼭질 2.java
+++ week9-tree+알파/taxol/bj_12851_숨바꼭질 2.java
@@ -7,55 +7,65 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int n, result;
-	static List<List<Integer>> tree;
-	static StringBuilder sb;
+	static int n, k, result = 9999999, count;
 	static boolean[] vis;
-	static int[] parent;
 	public static void main(String[] args) throws IOException {
 		//System.setIn(new FileInputStream("res/mainInput.txt"));	//제출 할 때 주석해야함
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	n = stoi(br.readLine());
-    	StringTokenizer stk;
+		
+    	StringTokenizer stk = new StringTokenizer(br.readLine());
+    	n = stoi(stk.nextToken());
+    	k = stoi(stk.nextToken());
     	
-    	tree = new ArrayList<>();
+    	vis = new boolean[100001];
     	
-    	for(int i = 0; i <= n; i++) {
-    		tree.add(new ArrayList<>());
+    	BFS();
+    	if(n == k) {
+    		result = 0;
+    		count = 1;
     	}
-    	int a,b;
-    	for(int i = 0; i < n - 1; i++) {
-    		stk = new StringTokenizer(br.readLine());
-    		a = stoi(stk.nextToken());
-    		b = stoi(stk.nextToken());
-    		tree.get(a).add(b);
-    		tree.get(b).add(a);
-    	}
+    	System.out.println(result);
+    	System.out.println(count);
     	
-    	sb = new StringBuilder();
-    	parent = new int[n+1];
-    	vis = new boolean[n+1];
-    	DFS(1);
-    	
-    	for(int i = 2; i <= n; i++)
-    		sb.append(parent[i]).append("\n");
-    	
-    	System.out.println(sb.toString());
     	br.close();
 	}
 	
-	private static boolean DFS(int now) {
-		vis[now] = true;
-		for(int next : tree.get(now)) {
-			if(!vis[next]) {
-				parent[next] = now;
-				vis[next] = true;
-				DFS(next);
+	static void BFS() {
+		
+		Queue<int[]> queue = new LinkedList<int[]>();
+		queue.add(new int[] {n, 0});
+		int[] q;
+		int curN, time;
+		vis[n] = true;
+		while(!queue.isEmpty()) {
+			q = queue.poll();
+			curN = q[0];
+			time = q[1];
+			
+			vis[curN] = true;
+			
+			if(curN == k) {
+				if(time < result) {
+					count = 1;
+					result = time;
+				}
+				else if(time == result){
+					count++;
+				}
 			}
+			
+			
+			if(curN + 1 <= 100000 && !vis[curN + 1]) 
+				queue.add(new int[] {curN + 1, time + 1});
+				
+			if(curN - 1 >= 0 && !vis[curN - 1])
+				queue.add(new int[] {curN - 1, time + 1});
+			
+			if(curN * 2 <= 100000 && !vis[curN * 2])
+				queue.add(new int[] {curN * 2, time + 1});
 		}
-		return false;
-	}
-
+	} 
+	
 	static int stoi(String str) {
     	return Integer.parseInt(str);
     }
