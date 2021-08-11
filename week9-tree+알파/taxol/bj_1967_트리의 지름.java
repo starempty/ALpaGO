d9c71c3a3f7884183d63bfd3c2c394fec6f438ab
한주연 <taxol1203@naver.com>
2021-08-11 18:11:29 +0900

    123

--- week9-tree+알파/taxol/bj_1967_트리의 지름.java
+++ week9-tree+알파/taxol/bj_1967_트리의 지름.java
@@ -7,56 +7,73 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int n, result;
-	static List<List<Integer>> tree;
-	static StringBuilder sb;
-	static boolean[] vis;
-	static int[] parent;
+	static int n, result, max, deep;
+	static List<List<Node>> node;
 	public static void main(String[] args) throws IOException {
 		//System.setIn(new FileInputStream("res/mainInput.txt"));	//제출 할 때 주석해야함
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
     	n = stoi(br.readLine());
     	StringTokenizer stk;
     	
-    	tree = new ArrayList<>();
+    	node = new ArrayList<>();
     	
     	for(int i = 0; i <= n; i++) {
-    		tree.add(new ArrayList<>());
-    	}
-    	int a,b;
-    	for(int i = 0; i < n - 1; i++) {
-    		stk = new StringTokenizer(br.readLine());
-    		a = stoi(stk.nextToken());
-    		b = stoi(stk.nextToken());
-    		tree.get(a).add(b);
-    		tree.get(b).add(a);
+    		node.add(new ArrayList<Node>());
     	}
     	
-    	sb = new StringBuilder();
-    	parent = new int[n+1];
-    	vis = new boolean[n+1];
-    	DFS(1);
-    	
-    	for(int i = 2; i <= n; i++)
-    		sb.append(parent[i]).append("\n");
-    	
-    	System.out.println(sb.toString());
+    	int a,b,c;
+		for(int i = 0; i < n - 1; i++) {
+			stk = new StringTokenizer(br.readLine());
+			a = stoi(stk.nextToken());
+			b = stoi(stk.nextToken());
+			c = stoi(stk.nextToken());
+			
+			node.get(a).add(new Node(b,c));
+			node.get(b).add(new Node(a,c));
+		}
+		// 루트 노드, 이전 노드, 현재까지의 합
+		DFS(1, -1, 0);
+		max = 0;
+		DFS(deep, -1, 0);
+		
+		System.out.println(max);
     	br.close();
 	}
 	
-	private static boolean DFS(int now) {
-		vis[now] = true;
-		for(int next : tree.get(now)) {
-			if(!vis[next]) {
-				parent[next] = now;
-				vis[next] = true;
-				DFS(next);
+	static void DFS(int curN, int preV, int sum) {
+		
+		if(max < sum) {
+			max = sum;
+			deep = curN;
+		}
+		
+		int size = node.get(curN).size();
+		for(int i = 0; i < size; i++) {
+			if(node.get(curN).get(i).getName() != preV) {
+				DFS(node.get(curN).get(i).getName(), curN, sum + node.get(curN).get(i).getVal());
 			}
 		}
-		return false;
+		
 	}
-
+	
 	static int stoi(String str) {
     	return Integer.parseInt(str);
     }
+	
+	static class Node{
+		private int name;
+		private int val;
+		
+		Node(int name, int val){
+			this.name = name;
+			this.val = val;
+		}
+		public int getName() {
+			return name;
+		}
+		public int getVal() {
+			return val;
+		}
+	}
 }
